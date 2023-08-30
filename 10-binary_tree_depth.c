1f45098aecc422f75e229b02930046edd95fa390
= <=>
2023-08-30 02:04:02 -0700

    TWO FIXED

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,9 +8,9 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 /* success if it = 0 */
-nt success = 0;
+
 	if (tree == NULL || tree->parent == NULL)
-		return (success);
+		return (0);
 	else
 		return (1 + binary_tree_depth(tree->parent));
 }
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,6 +13,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->right != NULL || tree->left != NULL)
 		return  (1 + binary_tree_nodes(tree->right) +
 			 binary_tree_nodes(tree->left));
-	return (success);
+	return (0);
 }
 
